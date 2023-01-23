691f243b2dab8a0d480a2cc99cf15424986d18e9
Michael Murray <mmurray24@hotmail.com>
2023-01-23 19:31:51 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,21 @@
+number = 2
+string = 'new string'
+float = 2.75
+
+first_name = 'Michael'
+last_name = 'Murray'
+
+full_name = first_name + ' ' + last_name
+
+print(full_name)
+
+
+day = ''
+def watching_arsenal(day):
+    if day == ('sunday'):
+        print('its time to watch Arsenal win 3 points')
+    else:
+        print('Arsenal are not playing today, wait until sunday')
+
+
+watching_arsenal('sunday')
